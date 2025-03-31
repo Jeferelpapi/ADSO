d7ee4d5614450fae17dad4af0c66cb8d0cbf673d
APRENDIZ <APRENDIZ@STMAPRRMDFSP608.FormacionSena.RED>
2025-03-31 17:38:29 -0500

    APPTURISMO

--- proyecto1/src/main/java/proyecto1/Main.java
+++ proyecto1/src/main/java/proyecto1/Main.java
@@ -1,9 +1,31 @@
 package proyecto1;
+import java.util.List;
 
-public class Factura {
-	
-	
-	
-	
+public class Main {
+    public static void main(String[] args) {
+        int num = 10;
+        int suma = 0;
 
+        for (int i = 1; i <= num; i++) {
+            suma += 1;
+            System.out.print(suma + " ");
+        }
+        System.out.println();
+
+        int[] array = {27, 78, 5, 36, 8};
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+
+        for (int a : array) {
+            System.out.print(a + " ");
+        }
+        System.out.println();
+
+        List<String> frutas = List.of("Pera", "Mango", "Manzanas", "Fresas");
+        for (String i : frutas) {
+            System.out.print(i + " ");
+        }
+    }
 }
